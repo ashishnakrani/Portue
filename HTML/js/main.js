611fbd1e1adb3de9af228658e3e8340bd4f751e8
Nakrani <ashish.nakrani.60@gmail.com>
2021-01-19 23:29:47 +0530

    feat: added home page Ui.

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,1 +1,13 @@
-// navbar
+"use stric";
+
+$(document).ready(function() {
+  $(document).on('click', '.nav-link', function() {
+    $('.navbar-indicator').css({width: '9rem'});
+    let currentEle = $(this);
+    let pos = currentEle.position().left;
+    $('.navbar-indicator').css({opacity:1, left: pos});
+    setTimeout(function() {
+      $('.navbar-indicator').css({width: '7rem'});
+    }, 100);
+  });
+});
