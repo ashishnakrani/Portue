2d1e2fce4e77fecb850f3a5ed403f592c00ee74c
Nakrani <ashish.nakrani.60@gmail.com>
2021-01-29 23:03:50 +0530

    fix: fix responsiveness issue and add doc.

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,54 +1,72 @@
 "use strict";
 
-$(document).ready(function() {
-  $(document).on('click', '.nav-link', function() {
-    $('.navbar-indicator').css({width: '9rem'});
+$(document).ready(function () {
+
+  // Navbar JS
+  $(document).on('click', '.nav-link', function () {
+    let navIndicatorWidth = '7rem';
+    let currentWindowWidth = $(window).width();
+    if (currentWindowWidth < 1024 && currentWindowWidth > 576) {
+      navIndicatorWidth = '4rem';
+    } else if (currentWindowWidth < 577) {
+      navIndicatorWidth = '3.5rem';
+    }
+    $(this).addClass('active');
+    $('.navbar-indicator').css({
+      width: navIndicatorWidth + 2
+    });
     let currentEle = $(this);
     let pos = currentEle.position().left;
-    $('.navbar-indicator').css({opacity:1, left: pos});
-    setTimeout(function() {
-      $('.navbar-indicator').css({width: '7rem'});
+    $('.navbar-indicator').css({
+      opacity: 1,
+      left: pos
+    });
+    setTimeout(function () {
+      $('.navbar-indicator').css({
+        width: navIndicatorWidth
+      });
     }, 100);
   });
 
-  $(document).ready(function(){
+  // Testimonials slider
   $(".testimonials").owlCarousel({
-    loop:true,
-    margin:30,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:2
-        }
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 2
+      }
     }
-});
   });
 });
 
-$(document).on('click', '#nav-about-me-tab', function() {
+$(document).on('click', '#nav-about-me-tab', function () {
   let skills = document.querySelectorAll('.item');
   let i;
-  for (i = 1; i<=skills.length; i++) {
-    let percentage = $('.skills__item'+i).attr('data-percentage');
-    $('.skills__item'+i).find('small b').remove();
-    $('.skills__item'+i).find('small br').remove();
-    $('.skills__item'+i).find('small').prepend(`<b>${percentage}%</b><br>`);  
-    $('.skills__item'+i).circleProgress({
-      value: parseFloat(percentage/100),
-      fill: {color: '#1D2B64'}
+  for (i = 1; i <= skills.length; i++) {
+    let percentage = $('.skills__item' + i).attr('data-percentage');
+    $('.skills__item' + i).find('small b').remove();
+    $('.skills__item' + i).find('small br').remove();
+    $('.skills__item' + i).find('small').prepend(`<b>${percentage}%</b><br>`);
+    $('.skills__item' + i).circleProgress({
+      value: parseFloat(percentage / 100),
+      fill: {
+        color: '#1D2B64'
+      }
     });
   }
 });
 
 // Project Masonry
 let $projects = $('.projects__grid');
-$projects.imagesLoaded(function() {
+$projects.imagesLoaded(function () {
   setTimeout(function () {
     $projects.isotope({
       itemSelector: '.projects__item',
@@ -58,27 +76,34 @@ $projects.imagesLoaded(function() {
         category: '[data-category]',
       }
     });
-  }, 2000);
+  }, 3000);
 });
-    
+
 // Masonry Filter
 let filterFns = {
   // show if number is greater than 50
-  numberGreaterThan50: function() {
+  numberGreaterThan50: function () {
     let number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
+    return parseInt(number, 10) > 50;
   },
   // show if name ends with -ium
-  ium: function() {
+  ium: function () {
     let name = $(this).find('.name').text();
-    return name.match( /ium$/ );
+    return name.match(/ium$/);
   }
 };
-$('button').on('click', function() {
-    let filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $projects.isotope({ filter: filterValue });
-    $(this).siblings().removeClass('active');
-    $(this).addClass('active');
+
+$('button').on('click', function () {
+  let filterValue = $(this).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[filterValue] || filterValue;
+  $projects.isotope({
+    filter: filterValue
+  });
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+});
+
+$(window).resize(function () {
+  $('.nav-link.active').trigger('click');
 });
